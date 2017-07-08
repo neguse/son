dc1d295eb9dd8b9056ad30bac5d1e1bf6f79bd8d
neguse <negcee@gmail.com>
2017-07-08 21:47:13 +0900

    Add docker.

---
+++ docker/bin/build.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -ex
+
+export HOME=/volume
+export GOROOT=/usr/local/go
+export GOPATH=/volume/go
+export NODEROOT=/usr/local/node-v8.1.3-linux-x64
+export PATH=$PATH:$NODEROOT/bin:$GOROOT/bin:$GOPATH/bin/
+
+REPO=github.com/neguse/son
+
+go get -u $REPO || echo "get"
+cd $GOPATH/src/$REPO
+cd client
+elm make Main.elm --yes --output ../assets/main.js
+cd ..
+
+go-bindata-assetfs assets/...
+mv bindata_assetfs.go server/
+
+cd server
+go get -u .
+go install
