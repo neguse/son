d9315ff29c51447c121fe0b80d4a34c9de5f4e0c
neguse <negcee@gmail.com>
2017-07-08 19:08:37 +0900

    2

--- docker/bin/build.sh
+++ docker/bin/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -ex
 
+GOROOT=/usr/local/go
+GOPATH=/volume/go
+NODEROOT=/usr/local/node-v8.1.3-linux-x64
+PATH=$PATH:$NODEROOT/bin:$GOROOT/bin:$GOPATH/bin/
+
 REPO=github.com/neguse/son
 
 go get -u https://$REPO
